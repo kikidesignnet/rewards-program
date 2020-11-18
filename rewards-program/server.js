5ac9c1281a5a95c979cf70adf92c1a5973768e57
kikibres <kristinabressler@yahoo.com>
2020-11-18 05:16:54 -0600

    update

---
+++ rewards-program/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require("path")
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+
+const customersRouter = require('./routes/customers');
+
+app.use('/customers', customersRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
